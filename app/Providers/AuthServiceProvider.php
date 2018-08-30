27145173faa9873e53191c9ec56f6b4e7a0c8cb4
stone <august_seki@163.com>
2018-08-30 14:11:01 +0800

    User can create topic

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,7 +26,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
     }
 }
